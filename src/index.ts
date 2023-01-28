2095a6608d1fb56626f9e402791f289fc2e05160
Alexis Leite <alexisleite.uy@gmail.com>
2023-01-28 16:52:42 -0300

    finished initial types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import PStore from './pstore';
 import registerStore from './registerStore';
 import useSelector from './useSelector';
 
-export { registerStore, useSelector, PStore };
+export * from './types';
+export { registerStore, useSelector };
+export default PStore;
 
