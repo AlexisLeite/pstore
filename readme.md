7d252ceb0b1816099c9c8dca04919865f9f96c2e
Alexis Leite <alexisleite.uy@gmail.com>
2023-01-27 23:58:40 -0300

    some tsup confs

---
+++ readme.md
@@ -0,0 +1,3 @@
+# PStore
+
+Pstore busca ser un contenedor de valores con posibilidad de suscripción a cambios, buscando lograr la mayor performance en cualquier escenario.
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,23 @@
 import type { Options } from 'tsup';
 
+const env = process.env.NODE_ENV
+
+
 const config: Options = {
+  bundle: env === 'production',
+  clean: true,
   dts: true,
   entry: [
-    'src/index.ts'
+    'src/**/*.ts'
   ],
   format: ['esm', "cjs"],
+  minify: env === 'production',
+  outDir: env === 'production' ? 'lib' : 'dist',
+  skipNodeModulesBundle: true,
   sourcemap: true,
+  splitting: true,
+  target: "es2020",
+  watch: env === 'development',
 }
 
 export default config
