86de4bf070fa3cd0617d046bc60133c102f313fe
Alexis Leite <alexisleite.uy@gmail.com>
2023-01-28 01:02:31 -0300

    PStore first code

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export type TId = string | number;
 
-export type TMap<T = string> = Record<TId, T>;
+export type TMap<T = unknown> = Record<TId, T>;
 
 export type TSuscriptor<T extends TMap = TMap> = (newProps: T) => unknown;
